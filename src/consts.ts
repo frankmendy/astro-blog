1c8cde6fa679e81bb108f0235fe736cc84416fae
frankmendy <121290171+frankmendy@users.noreply.github.com>
2026-06-30 23:28:15 +0800

    Update site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
+// Place any global data here.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Gansu Odyssey';
+export const SITE_DESCRIPTION = 'The Ultimate Visual Guide to China\'s Silk Road, Ancient Grottos, and Rainbow Mountains.';
